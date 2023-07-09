e5947b7079605123a66c3f83de37abf328929438
Tomasz Babiuk <tomasz.babiuk@gmail.com>
2023-07-09 09:54:05 +0200

    adding blinky plugin

---
+++ build-plugins.sh
@@ -0,0 +1,18 @@
+rm -rf ../automate-everything/output/plugins/*
+../aeplugin-afore/gradlew :aeplugin-afore:buildForAutomateEverything
+../aeplugin-afore/gradlew :aeplugin-alarm:buildForAutomateEverything
+../aeplugin-afore/gradlew :aeplugin-bashaction:buildForAutomateEverything
+../aeplugin-afore/gradlew :aeplugin-centralheating:buildForAutomateEverything
+../aeplugin-afore/gradlew :aeplugin-cryptotrading:buildForAutomateEverything
+../aeplugin-afore/gradlew :aeplugin-emailaction:buildForAutomateEverything
+../aeplugin-afore/gradlew :aeplugin-homeautomationicons:buildForAutomateEverything
+../aeplugin-afore/gradlew :aeplugin-mobileaccess:buildForAutomateEverything
+../aeplugin-afore/gradlew :aeplugin-onewire:buildForAutomateEverything
+../aeplugin-afore/gradlew :aeplugin-onoff:buildForAutomateEverything
+../aeplugin-afore/gradlew :aeplugin-scenes:buildForAutomateEverything
+../aeplugin-afore/gradlew :aeplugin-sensorsandcontrollers:buildForAutomateEverything
+../aeplugin-afore/gradlew :aeplugin-shelly:buildForAutomateEverything
+../aeplugin-afore/gradlew :aeplugin-time:buildForAutomateEverything
+../aeplugin-afore/gradlew :aeplugin-zigbee2mqtt:buildForAutomateEverything
+../aeplugin-afore/gradlew :aeplugin-porttester:buildForAutomateEverything
+../aeplugin-afore/gradlew :aeplugin-blinky:buildForAutomateEverything
